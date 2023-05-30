ac778e221a85046ed3f1e2eaa9089395ce7c6737
JW123 <JW123>
2023-05-30 15:34:19 +0800

    feat: 两数之和

--- Day2-两数相加/addTwoNumbers.ts
+++ Day2-两数相加/addTwoNumbers.ts
@@ -1,39 +1,62 @@
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
+/**解题思路 */
+//自解：两次循坏，第二个for循坏下标始终是第一个for下标+1，满足条件则push进结果数组，最后去重
+//官解：(哈希表)一次循环，利用map，在map.set用目标值减去当前数组值作为key，当前下标作为value，如果在map里面找到当前值，则返回
+/**自己实现 */
+// function twoSum(nums: number[], target: number): number[] {
+//   let resultArr: number[] = []
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] == target) {
+//         resultArr.push(i)
+//         resultArr.push(j)
+//       }
+//     }
+//   }
+//   resultArr = [...new Set(resultArr)]
+//   return resultArr
+// };
 
-function addTwoNumbers(
-  l1: ListNode | null,
-  l2: ListNode | null
-): ListNode | null {
-  if (!l1 || !l2) {
-    return null;
-  }
-  let change = 0,
-    addNodeList,
-    currentNodeList;
-  while (l1 || l2) {
-    let val1 = l1 ? l1.val : 0;
-    let val2 = l2 ? l2.val : 0;
-    let addNodeNum = val1 + val2 + change;
-    if (!addNodeList) {
-      currentNodeList = new ListNode(addNodeNum);
-      addNodeList = currentNodeList;
+/**官方实现 */
+function twoSum(nums: number[], target: number): number[] {
+  let map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return [map.get(nums[i]), i]
     } else {
-      currentNodeList.next = new ListNode(addNodeNum);
-      currentNodeList = currentNodeList.next;
+      map.set(target - nums[i], i)
     }
-    change = Math.floor(addNodeNum / 10);
-    l1 = l1 ? l1.next : null;
-    l2 = l2 ? l2.next : null;
   }
-  if (change > 0) {
-    currentNodeList.next = new ListNode(1);
-  }
-  return addNodeList;
-}
+  return []
+};
+
+/**运行用例 */
+console.log(twoSum([2, 7, 11, 15, 5, 2], 9))
+
+/**其他实现方法 */
+// 利用双指针
+// function twoSum(nums: number[], target: number): number[] {
+//   const results = []
+//   const numsWithIndex = nums
+//     .map((num, index) => {
+//       return {
+//         value: num,
+//         index,
+//       }
+//     }).sort((numOne, numTwo) => numOne.value - numTwo.value)
+//   let left = 0
+//   let right = numsWithIndex.length - 1
+//   while (left < right) {
+//     const sum = numsWithIndex[left].value + numsWithIndex[right].value
+//     if (sum < target) {
+//       left++
+//     } else if (sum > target) {
+//       right--
+//     } else {
+//       return [
+//         Math.min(numsWithIndex[left].index, numsWithIndex[right].index),
+//         Math.max(numsWithIndex[left].index, numsWithIndex[right].index)
+//       ]
+//     }
+//   }
+//   return []
+// };
